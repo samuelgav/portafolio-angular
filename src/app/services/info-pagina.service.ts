80fbc2d1bcca481986153cef6396882502851122
samuelgav <samuelgavidia@upeu.edu.pe>
2019-01-29 11:49:36 -0500

    cuarto commit

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -7,13 +8,13 @@ import { HttpClient } from '@angular/common/http';
 
 export class InfoPaginaService {
 
-  info: any = {};
+  info: InfoPagina = {};
   cargada = false;
 
   constructor(private http: HttpClient) {
     // console.log('servicio de infopagina listo');
     this.http.get('assets/data/data-pagina.json')
-        .subscribe( resp => {
+        .subscribe( (resp: InfoPagina) => {
         // console.log(resp);
         this.cargada = true;
         this.info = resp;
