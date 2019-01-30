a905f26f2e4d4100c9e5aa5adc4b74dabfd3a6ba
samuelgav <samuelgavidia@upeu.edu.pe>
2019-01-30 17:25:12 -0500

    nuevo commit

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -11,14 +11,31 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor(private http: HttpClient) {
+     this.cargarInfo();
+     this.cargarEquipo();
+  }
+
+  private cargarInfo() {
     // console.log('servicio de infopagina listo');
     this.http.get('assets/data/data-pagina.json')
         .subscribe( (resp: InfoPagina) => {
         // console.log(resp);
         this.cargada = true;
         this.info = resp;
-        console.log(resp);
         });
   }
+
+  private cargarEquipo() {
+    // console.log('servicio de infopagina listo');
+    this.http.get('https://angular-html-b061a.firebaseio.com/equipo.json')
+        .subscribe( (resp: any[]) => {
+        // console.log(resp);
+        this.equipo = resp;
+        // console.log(resp);
+      });
+  }
+
 }
